b80487b84d746c2af65e0453783556a32209e5cf
Naohide Sano <namachapanda2005@gmail.com>
2022-09-08 05:09:03 +0900

    [cocoa-basic] implement jsr223

---
+++ src/main/java/net/sf/cocoa/jsr223/BasicScriptEngineFactory.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2008 by Naohide Sano, All rights reserved.
+ *
+ * Programmed by Naohide Sano
+ */
+
+package net.sf.cocoa.jsr223;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import javax.script.ScriptEngineFactory;
+
+import vavi.util.Debug;
+
+
+/**
+ * BasicScriptEngineFactory.
+ *
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (nsano)
+ * @version 0.00 220726 nsano make the initial version <br>
+ */
+public class BasicScriptEngineFactory implements ScriptEngineFactory {
+
+    /** */
+    private static final String FILEEXT = ".bas";
+
+    /** */
+    private static final String [] MIMETYPES = {
+        "text/plain",
+        "text/x-basic",
+        "application/x-basic"
+    };
+
+    /** */
+    private static final String [] NAMES = {
+        "CocoaBasic",
+        "basic"
+    };
+
+    /** */
+    private BasicScriptEngine scriptEngine;
+    /** */
+    private List<String> extensions;
+    /** */
+    private List<String> mimeTypes;
+    /** */
+    private List<String> names;
+
+    /** */
+    public BasicScriptEngineFactory() {
+        scriptEngine = new BasicScriptEngine(this);
+        extensions = Collections.nCopies(1, FILEEXT);
+        mimeTypes = Arrays.asList(MIMETYPES);
+        names = Arrays.asList(NAMES);
+    }
+
+    @Override
+    public String getEngineName() {
+        return getScriptEngine().get(BasicScriptEngine.ENGINE).toString();
+    }
+
+    @Override
+    public String getEngineVersion() {
+        return getScriptEngine().get(BasicScriptEngine.ENGINE_VERSION).toString();
+    }
+
+    @Override
+    public List<String> getExtensions() {
+        return extensions;
+    }
+
+    @Override
+    public List<String> getMimeTypes() {
+        return mimeTypes;
+    }
+
+    @Override
+    public List<String> getNames() {
+        return names;
+    }
+
+    @Override
+    public String getLanguageName() {
+        return getScriptEngine().get(BasicScriptEngine.LANGUAGE).toString();
+    }
+
+    @Override
+    public String getLanguageVersion() {
+        return getScriptEngine().get(BasicScriptEngine.LANGUAGE_VERSION).toString();
+    }
+
+    @Override
+    public Object getParameter(String key) {
+        return getScriptEngine().get(key).toString();
+    }
+
+    @Override
+    public String getMethodCallSyntax(String obj, String m, String... args)  {
+        StringBuilder sb = new StringBuilder();
+        sb.append("(").append(obj).append(" ").append(m);
+        int len = args.length;
+        for (int i = 0; i < len; i++) {
+            if (i > 0) {
+                sb.append(' ');
+            }
+            sb.append(args[i]);
+        }
+        sb.append(")");
+        return sb.toString();
+    }
+
+    @Override
+    public String getOutputStatement(String toDisplay) {
+        return "print \"" + toDisplay + "\";";
+    }
+
+    @Override
+    public String getProgram(String ... statements) {
+        StringBuilder sb = new StringBuilder();
+        int len = statements.length;
+        for (int i = 0; i < len; i++) {
+            if (i > 0) {
+                sb.append('\n');
+            }
+            sb.append(statements[i]);
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public BasicScriptEngine getScriptEngine() {
+        return scriptEngine;
+    }
+}
+
+/* */
--- src/test/java/net/sf/cocoa/Test1.java
+++ src/test/java/net/sf/cocoa/Test1.java
@@ -8,7 +8,13 @@ package net.sf.cocoa;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptEngineManager;
+
+import net.sf.cocoa.jsr223.BasicScriptEngineFactory;
 import org.junit.jupiter.api.Test;
 
 import vavi.util.Debug;
@@ -42,6 +48,45 @@ Debug.println("done");
         CommandInterpreter ci = new CommandInterpreter(is, os);
         ci.start(true);
 Debug.println("done");
+    }
+
+    @Test
+    public void test0() throws Exception {
+        ScriptEngineManager sem = new ScriptEngineManager();
+        List<ScriptEngineFactory> list = sem.getEngineFactories();
+
+        for (int i = 0; i < list.size(); i++) {
+            ScriptEngineFactory f = list.get(i);
+
+            String engineName = f.getEngineName();
+            String engineVersion = f.getEngineVersion();
+            String langName = f.getLanguageName();
+            String langVersion = f.getLanguageVersion();
+            System.out.println("\n---- " + i + " ----\n" + engineName + " " +
+                    engineVersion + " (" +
+                    langName + " " +
+                    langVersion + ")");
+        }
+    }
+
+    @Test
+    void testJsr223() throws Exception {
+        ScriptEngineManager sem = new ScriptEngineManager();
+        ScriptEngine engine = sem.getEngineByName("basic");
+Debug.println("engine: " + engine);
+
+        String statement =
+                "100 FOR i = 1 to 10\n" +
+                "110 FOR j = i to 10\n" +
+                "120 PRINT \"*\";\n" +
+                "130 NEXT j\n" +
+                "140 NEXT i";
+        Object result = engine.eval(statement);
+Debug.println("result: " + result);
+
+        statement = "100 LET A = 1 + 100";
+        result = engine.eval(statement);
+Debug.println("result: " + result);
     }
 }
 
